f93f2ad8a7202fc1348187bdd11fd306c674b02c
Adeeb HS <adeeb.hs1@gmail.com>
2021-07-30 21:00:26 +0400

    update'

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = '(:guessTheKey:)'
     JWT_SECRET_KEY = '(:guessTheKey_JWTVersion:)'
 
-    DB_NAME = os.environ['DATABASE_URL']
+    DB_NAME = 'postgres://dfwtnnmybieilm:c3df2dbc7edcfb92c93239bb23e6287c55875930a8ce8064bac02bfcf214c56e@ec2-18-214-140-149.compute-1.amazonaws.com:5432/d70si98fvuu3g3'
     SESSION_TYPE = 'filesystem'
     JWT_ACCESS_TOKEN_EXPIRES = False
 
